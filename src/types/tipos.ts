5d46e9a62c34f0658c2d4ffb002e169dfaa25c12
Francesco Sidotti <francesco.sidotti12@gmail.com>
2026-03-04 19:52:44 -0300

    POJO for the attribute types

---
+++ src/types/tipos.ts
@@ -0,0 +1,11 @@
+export const AttributeType = {
+  1:"Text",
+  2:"Number",
+  3:"Long Text",
+  4:"Select",
+  5:"Confirmation",
+  6:"Image",
+  7:"Location",
+} as const;
+
+export type AttributeType = typeof AttributeType[keyof typeof AttributeType];
